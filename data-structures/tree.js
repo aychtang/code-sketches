8d8fcc04f8e890ea948b6233cbb6c6b493ad28a7
Howard Tang <tangh@google.com>
2014-01-23 12:46:47 +0000

    Adds recursive bst.

---
+++ data-structures/tree.js
@@ -0,0 +1,22 @@
+
+var makeTree = function(parent, item) {
+	return {
+		item: item,
+		parent: parent,
+		left: null,
+		right: null
+	};
+};
+
+var find = function(tree, item) {
+	if (item == tree.item) {
+		return tree;
+	}
+
+	if (item < tree.item && tree.left) {
+		return find(tree.left, item);
+	}
+	else if (tree.right) {
+		return find(tree.right, item);
+	}
+};
