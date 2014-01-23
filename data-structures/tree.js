d301d0a008582093b92d48b00514fe5389bced3e
Howard Tang <tangh@google.com>
2014-01-23 16:21:13 +0000

    Adds insert helper.

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -20,3 +20,19 @@ var find = function(tree, item) {
 		return find(tree.right, item);
 	}
 };
+
+var insert = function(tree, item) {
+	if (item < tree.item) {
+		if (!tree.left)
+			tree.left = makeTree(tree, item);
+		else
+			insert(tree.left, item);
+	}
+
+	else if (item >= tree.item) {
+		if (!tree.right)
+			tree.right = makeTree(tree, item);
+		else
+			insert(tree.right, item);
+	}
+};
