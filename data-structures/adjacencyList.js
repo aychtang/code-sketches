cf41eba31e721ada418ee2dc8408bd5432dbc79b
Howard Tang <tangh@google.com>
2014-01-28 15:37:01 +0000

    Adds graph code.

---
+++ data-structures/adjacencyList.js
@@ -0,0 +1,33 @@
+
+var nextId = 0;
+
+// Recursive graph structure.
+// Each member of the graph is stored in the nodeset of the head graph node.
+// Each edge of a node is stored in the nodeset of its asscociated node.
+
+var makeGraph = function(data) {
+
+	return {
+		id: ++nextId,
+		amount: 0,
+		nodeSet: {},
+		nodes: [],
+		data : data || {}
+	};
+
+};
+
+var addNode = function(graph, node) {
+
+	if (!graph.nodeSet[node.id]) {
+		graph.nodeSet[node.id] = node;
+		graph.nodes.push(node);
+	}
+
+};
+
+var getNode = function(graph, id) {
+
+	return graph.nodeSet[id];
+
+};
