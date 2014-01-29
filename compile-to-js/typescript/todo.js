41f9170d5c3a27367666ac6b272bc0cdf49d1ea9
Cheeseen <aychtang@gmail.com>
2014-01-29 23:15:28 +0000

    Working render fn and list class.

--- compile-to-js/typescript/todo.js
+++ compile-to-js/typescript/todo.js
@@ -1,5 +1,29 @@
-function log(todo) {
+var TodoList = (function () {
+    function TodoList() {
+        this.todos = [];
+    }
+    TodoList.prototype.add = function (todo) {
+        this.todos.push(todo);
+    };
+    return TodoList;
+})();
+
+function getTask(todo) {
     return todo.task;
 }
 
-console.log(log({ task: 'hello world', done: false }));
+function renderItem(el, item) {
+    var newNode = document.createElement('li');
+    newNode.innerText = item.task;
+    el.appendChild(newNode);
+}
+
+function renderList(list) {
+    list.todos.forEach(function (item) {
+        renderItem(document.getElementById('list'), item);
+    });
+}
+
+var list = new TodoList();
+list.add({ task: 'hello mom', done: false });
+renderList(list);
--- compile-to-js/typescript/todo.ts
+++ compile-to-js/typescript/todo.ts
@@ -1,4 +1,14 @@
 
+	class TodoList {
+		todos: Todo[];
+		constructor () {
+			this.todos = [];
+		}
+		add(todo : Todo) {
+			this.todos.push(todo);
+		}
+	}
+
 	interface Todo {
 		task: string;
 		done: boolean;
@@ -8,6 +18,18 @@
 		return todo.task;
 	}
 
-	console.log(
-		getTask({task:'hello world', done:false})
-	);
+	function renderItem(el, item: Todo) {
+		var newNode = document.createElement('li');
+		newNode.innerText = item.task;
+		el.appendChild(newNode);
+	}
+
+	function renderList(list: TodoList) {
+		list.todos.forEach(function(item: Todo) {
+			renderItem(document.getElementById('list'), item);
+		});
+	}
+
+	var list = new TodoList();
+	list.add({task:'hello mom', done:false});
+	renderList(list);
