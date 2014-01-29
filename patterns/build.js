011e835a6c518556ce14b33a89ad4d51af80b8da
Howard Tang <tangh@google.com>
2014-01-29 14:20:59 +0000

    Adds build pattern.

---
+++ patterns/build.js
@@ -0,0 +1,14 @@
+
+// Build function runs init method on a module and returns it.
+// Allowing you to init modules and keep a reference to them in one line.
+
+var build = function(module) {
+	module.init();
+	return module;
+};
+
+var App = function() {};
+
+App.init = function() {
+	this.module = build(new App.module());
+};
