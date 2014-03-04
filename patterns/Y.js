aa00a3e0e46637d7653c5d4d3a7e97ce9bde699d
Howard Tang <tangh@google.com>
2014-03-04 10:02:36 +0000

    Adds a memoized Y-like combinator.

--- patterns/Y.js
+++ patterns/Y.js
@@ -4,6 +4,18 @@ var Y = function(f) {
 	};
 };
 
+var Ymem = function(f, cache) {
+	var cache = cache || {};
+	return function(n) {
+		if (!cache[n]) {
+			cache[n] = f(function(n) {
+				return (Ymem(f, cache)(n));
+			})(n);
+		}
+		return cache[n];
+	};
+};
+
 var factorial = Y(function(self) {
 	return function(n) {
 		if (n === 0) return 1;
@@ -15,5 +27,19 @@ var fib = Y(function(self) {
 	return function(n) {
 		if (n < 2) return n;
 		return self(self)(n - 1) + self(self)(n - 2);
+	};
+});
+
+var memFactorial = Ymem(function(self) {
+	return function(n) {
+		if (n === 0) return 1;
+		return n * self(n - 1);
+	};
+});
+
+var memFib = Ymem(function(self) {
+	return function(n) {
+		if (n < 2) return n;
+		return self(n - 1) + self(n - 2);
 	};
 });
